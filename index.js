bf0eb96305e2910cc03b04f1c5c33825a0971829
michaelmichio <michaelmichio.work@gmail.com>
2022-12-15 12:36:19 +0700

    update index html n js

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ import path from "path";
 const app = express();
 
 app.use(express.static(path.resolve('public')));
-app.set("view engine","ejs");
+
+app.set("view engine", "ejs");
+
 const pool = mysql.createPool({
     host:'localhost',
     user : 'root',
@@ -13,45 +15,48 @@ const pool = mysql.createPool({
     connectionLimit:10
 })
 
-const dbConnect = ()=>{
-    return new Promise((resolve,reject)=>{
-        pool.getConnection((err,conn)=>{
-            if(err){
+const dbConnect = () => {
+    return new Promise((resolve, reject) => {
+        pool.getConnection((err, conn) => {
+            if(err) {
                 reject(err);
             }
-            else{
+            else {
                 resolve(conn);
             }
         })
     })
 }
 
-const cariTopTenKarakter = (conn,masukan)=>{
-    return new Promise((resolve,reject)=>{
-        conn.query('SELECT source,sum(weight) as weight FROM interaksi WHERE book=? GROUP BY source ORDER BY sum(weight) DESC LIMIT 10',masukan,
-        (err,result)=>{
-            if(err){
-                reject(err);
+const cariTopTenKarakter = (conn, masukan) => {
+    return new Promise((resolve, reject) => {
+        conn.query(
+            'SELECT source,sum(weight) as weight FROM interaksi WHERE book=? GROUP BY source ORDER BY sum(weight) DESC LIMIT 10',
+            masukan,
+            (err, result) => {
+                if(err) {
+                    reject(err);
+                }
+                else {
+                    resolve(result);
+                }
             }
-            else{
-                resolve(result);
-            }
-        })
+        )
     })
 }
 
-app.get('/',async(req,res)=>{
-    res.sendFile('index.html',{root:'.'});
+app.get('/', async(req, res) => {
+    res.sendFile('index.html', {root:'.'});
 })
-app.get('/GrafikBar/',async(req,res)=>{
+
+app.get('/GrafikBar/', async(req, res) => {
     const conn = await dbConnect();
-    const hasil = await cariTopTenKarakter(conn,req.query.Buku);
-    const contoh ="A";
-    res.render('GrafikBar',{
+    const hasil = await cariTopTenKarakter(conn, req.query.Buku);
+    const contoh = "A";
+    res.render('GrafikBar', {
         hasil,
         buku:req.query.Buku
     })
 })
-
 
 app.listen(8080);
